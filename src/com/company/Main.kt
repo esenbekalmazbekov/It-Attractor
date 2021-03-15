dc5721249e5686ce1fbe0e4a6ec55f37e2155e3f
EsEnbek <esenbek.almazbekov@gmail.com>
2021-03-15 22:02:12 +0600

    task1

---
+++ src/com/company/Main.kt
@@ -0,0 +1,46 @@
+package com.company
+
+import kotlin.collections.ArrayList
+
+fun main() {
+    val daysPrice = ArrayList<Int>()
+
+    initDaysPrice(daysPrice)
+    daysPrice.addAll(arrayOf(1, 1, 3, 1, 2))
+
+    print("max profit = ${getMaxValue(daysPrice)}")
+}
+
+fun getMaxValue(daysPrice: ArrayList<Int>): Int {
+    if (daysPrice.size == 0) return 0
+    if (daysPrice.size == 1) return daysPrice[0]
+    if (daysPrice.size == 2) {
+        return if (daysPrice[0] > daysPrice[1]){
+            (daysPrice[0] * 2)
+        } else {
+            (daysPrice[1] * 2)
+        }
+    }
+    var profit = 0
+    var count = daysPrice[daysPrice.size - 1]
+    var max = daysPrice[daysPrice.size - 1]
+    for(i in daysPrice.size - 2 downTo 0) {
+        println("i = $i, price = ${daysPrice[i]}")
+        if (max >= daysPrice[i]) {
+            count += max
+        } else {
+            profit += (count + daysPrice[i])
+            max = daysPrice[i]
+            count = 0
+        }
+        println("count = $count")
+    }
+    profit += count
+    return profit
+}
+
+fun initDaysPrice(daysPrice: ArrayList<Int>) {
+    for (i in 0..99) {
+        daysPrice.add((Math.random() * 100).toInt())
+    }
+}
